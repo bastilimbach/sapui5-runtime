957fc333377d30494d6f0465a50e8fa88c3a8b62
Jakob Marius Kjaer <jakob.m.kjaer@gmail.com>
2019-11-07 11:35:20 +1000

    Update install.js
    Added StreamZip to handle extraction and tunnel for proxy agent

--- src/install.js
+++ src/install.js
@@ -1,134 +1,198 @@
 const fs = require('fs-extra')
 const path = require('path')
 const url = require('url')
-const AdmZip = require('adm-zip')
+const StreamZip = require('node-stream-zip');
+
 const axios = require('axios')
 const ProgressBar = require('progress')
+const tunnel = require('tunnel')
+var agent;
+if (process.env.HTTPS_PROXY) {
+	let https_proxy_object = process.env.HTTPS_PROXY.split("://");
+	proxyAuth = https_proxy_object[1].split("@")[0],
+		hostname = https_proxy_object[1].split("@")[1].split(":")[0],
+		port = https_proxy_object[1].split("@")[1].split(":")[1];
 
+	agent = tunnel.httpsOverHttp({
+		proxy: {
+			host: hostname,
+			port: port,
+			proxyAuth: proxyAuth
+		},
+	});
+}
 const packageJSONPath = path.resolve(`${__dirname}/../../../package.json`)
 let config
 try {
-  const packageJSON = require(packageJSONPath)
-  config = packageJSON['sapui5-runtime'] || {}
+	const packageJSON = require(packageJSONPath)
+	config = packageJSON['sapui5-runtime'] || {}
 } catch (error) {
-  config = {}
+	config = {}
 }
 
 async function prepareFileSystem(lib, download) {
-  await fs.remove(lib)
-  fs.mkdirp(lib)
-  await fs.remove(download)
-  fs.mkdirp(download)
+	await fs.remove(lib)
+	fs.mkdirp(lib)
+	await fs.remove(download)
+	fs.mkdirp(download)
 }
 
 async function determineLatestVersionURL(versionEndpoint, downloadEndpoint) {
-  console.log('Searching for latest SAPUI5 version...')
-  try {
-    const { data: versionData } = await axios.get(versionEndpoint)
-    const patchHistory = [
-      ...new Set([...versionData.libraries[0].patchHistory, versionData.version].reverse()),
-    ]
-    const testedVersions = []
-    /* eslint-disable no-await-in-loop */
-    for (let i = 0; i < patchHistory.length; i += 1) {
-      try {
-        const downloadURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${patchHistory[i]}.zip`)
-        await axios.get(downloadURL)
-        console.log(`SAPUI5 version ${patchHistory[i]} found.`)
-        return downloadURL
-      } catch (versionError) {
-        testedVersions.push({
-          version: patchHistory[i],
-          url: url.resolve(downloadEndpoint.href, `sapui5-rt-${patchHistory[i]}.zip`),
-        })
-      }
-    }
-    throw new Error(`Could not determine the available SAPUI5 version. Is the file 'sapui5-rt-{version}.zip' available at '${downloadEndpoint.href}'?
+	console.log('Searching for latest SAPUI5 version...')
+	try {
+		const {
+			data: versionData
+		} = await axios.get(versionEndpoint)
+		const patchHistory = [
+			...new Set([...versionData.libraries[0].patchHistory, versionData.version].reverse()),
+		]
+		const testedVersions = []
+		/* eslint-disable no-await-in-loop */
+		for (let i = 0; i < patchHistory.length; i += 1) {
+			try {
+				const downloadURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${patchHistory[i]}.zip`)
+				await axios.get(downloadURL)
+				console.log(`SAPUI5 version ${patchHistory[i]} found.`)
+				return downloadURL
+			} catch (versionError) {
+				testedVersions.push({
+					version: patchHistory[i],
+					url: url.resolve(downloadEndpoint.href, `sapui5-rt-${patchHistory[i]}.zip`),
+				})
+			}
+		}
+		throw new Error(`Could not determine the available SAPUI5 version. Is the file 'sapui5-rt-{version}.zip' available at '${downloadEndpoint.href}'?
     I tried the following versions: ${JSON.stringify(testedVersions, null, 2)}`)
-  } catch (error) {
-    throw new Error(`Could not receive SAPUI5 versions. Does the file 'sap-ui-version.json' exists at '${url.parse(versionEndpoint).href}'?`)
-  }
+	} catch (error) {
+		throw new Error(`Could not receive SAPUI5 versions. Does the file 'sap-ui-version.json' exists at '${url.parse(versionEndpoint).href}'?`)
+	}
 }
 
 async function downloadSAPUI5(downloadURL, downloadPath) {
-  console.warn('By using this npm package you agree to the EULA from SAP: https://tools.hana.ondemand.com/developer-license-3_1.txt/')
-  console.log('Downloading SAPUI5...')
-  const zipFile = path.join(downloadPath, 'sapui5.zip')
-  try {
-    const response = await axios.get(downloadURL, {
-      responseType: 'stream',
-      headers: {
-        Cookie: 'eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt',
-      },
-    })
+	console.warn('By using this npm package you agree to the EULA from SAP: https://tools.hana.ondemand.com/developer-license-3_1.txt/')
+	console.log('Downloading SAPUI5...')
+	const zipFile = path.join(downloadPath, 'sapui5.zip')
+	var response;
+	try {
+		if (agent) {
+			response = await axios.get(downloadURL, {
+				responseType: 'stream',
+				headers: {
+					Cookie: 'eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt',
+				},
+				httpsAgent: agent,
+				proxy: false
 
-    const progressBar = new ProgressBar('Downloading: [:bar] :rate/bps :percent :etas', {
-      complete: '=',
-      incomplete: ' ',
-      width: 20,
-      total: parseInt(response.headers['content-length'], 10),
-    })
+			})
+		} else {
+			response = await axios.get(downloadURL, {
+				responseType: 'stream',
+				headers: {
+					Cookie: 'eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt',
+				},
+			})
+		}
 
-    response.data.on('data', (data) => {
-      progressBar.tick(data.length)
-    })
+		const progressBar = new ProgressBar('Downloading: [:bar] :rate/bps :percent :etas', {
+			complete: '=',
+			incomplete: ' ',
+			width: 20,
+			total: parseInt(response.headers['content-length'], 10),
+		})
 
-    response.data.pipe(fs.createWriteStream(zipFile))
-    return new Promise((resolve, reject) => {
-      response.data.on('end', () => { resolve(zipFile) })
-      response.data.on('error', () => { reject() })
-    })
-  } catch (downloadError) {
-    throw new Error(`Couldn't download SAPUI5 zip archive from '${downloadURL}'`)
-  }
+		response.data.on('data', (data) => {
+			progressBar.tick(data.length)
+		})
+
+		response.data.pipe(fs.createWriteStream(zipFile))
+		return new Promise((resolve, reject) => {
+			response.data.on('end', () => {
+				resolve(zipFile)
+			})
+			response.data.on('error', () => {
+				reject()
+			})
+		})
+	} catch (downloadError) {
+		throw new Error(`Couldn't download SAPUI5 zip archive from '${downloadURL}'`)
+	}
 }
 
 async function extractArchive(archive, targetPath) {
-  console.log('Extracting...')
-  const zip = new AdmZip(archive)
-  zip.extractAllTo(targetPath, true)
+	console.log(`Extracting files to  ${targetPath}`)
+
+	const zip = new StreamZip({
+		file: archive,
+		storeEntries: true
+	});
+
+
+
+	zip.on('ready', () => {
+		console.log('All entries read: ' + zip.entriesCount);
+
+		const progressBar1 = new ProgressBar('Extracting: [:bar]', {
+
+			total: zip.entriesCount,
+		})
+		zip.on('extract', (entry, file) => {
+			progressBar1.tick();
+			if (progressBar1.complete) {
+				console.log('SAPUI5 Installed');
+			}
+		});
+		zip.extract(null, targetPath, (err, count) => {
+
+			zip.close();
+		});
+	});
 }
 
-const libDir = path.resolve(`${__dirname}/../lib`)
+const libDir = (config.dest) ? path.resolve(`${__dirname}/../../../`, config.dest) : path.resolve(`${__dirname}/../lib`)
 const downloadDir = path.resolve(`${__dirname}/../tmp`)
 const versionEndpoint = url.parse('https://sapui5.hana.ondemand.com/resources/sap-ui-version.json')
 const downloadEndpoint = url.parse('https://tools.hana.ondemand.com/additional/')
 let latestVersionURL = ''
 
 async function installSAPUI5() {
-  try {
-    await prepareFileSystem(libDir, downloadDir)
+	try {
+		await prepareFileSystem(libDir, downloadDir)
 
-    const sapui5Archive = await downloadSAPUI5(latestVersionURL, downloadDir)
-    await extractArchive(sapui5Archive, libDir)
-  } catch (error) {
-    fs.remove(libDir)
-    throw error
-  }
-  fs.remove(downloadDir)
-  console.log('SAPUI5 successfully installed!')
+		await downloadSAPUI5(latestVersionURL, downloadDir).then((sapui5Archive) => {
+			extractArchive(sapui5Archive, libDir)
+		}).catch(function (error) {
+			console.log(error.message)
+		})
+
+	} catch (error) {
+		console.log(error)
+		fs.remove(libDir)
+		throw error
+	}
+	fs.remove(downloadDir)
+
 }
 
 (async () => {
-  if (!config.version) {
-    latestVersionURL = await determineLatestVersionURL(versionEndpoint, downloadEndpoint)
-  } else {
-    latestVersionURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${config.version}.zip`)
-  }
-  try {
-    const sapUIVersionFile = await fs.readFile(path.resolve(`${libDir}/resources/sap-ui-version.json`))
-    const parsedVersionFile = JSON.parse(sapUIVersionFile.toString('utf8'))
-    const installedSAPUI5VersionURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${parsedVersionFile.version}.zip`)
-    if (installedSAPUI5VersionURL === latestVersionURL) {
-      console.log(`SAPUI5 version ${parsedVersionFile.version} already installed.`)
-    } else {
-      await installSAPUI5()
-    }
-  } catch (readFileError) {
-    if (readFileError.code === 'ENOENT') {
-      await installSAPUI5()
-    } else {
-      throw readFileError
-    }
-  }
+	if (!config.version) {
+		latestVersionURL = await determineLatestVersionURL(versionEndpoint, downloadEndpoint)
+	} else {
+		latestVersionURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${config.version}.zip`)
+	}
+	try {
+		const sapUIVersionFile = await fs.readFile(path.resolve(`${libDir}/resources/sap-ui-version.json`))
+		const parsedVersionFile = JSON.parse(sapUIVersionFile.toString('utf8'))
+		const installedSAPUI5VersionURL = url.resolve(downloadEndpoint.href, `sapui5-rt-${parsedVersionFile.version}.zip`)
+		if (installedSAPUI5VersionURL === latestVersionURL) {
+			console.log(`SAPUI5 version ${parsedVersionFile.version} already installed.`)
+		} else {
+			await installSAPUI5()
+		}
+	} catch (readFileError) {
+		if (readFileError.code === 'ENOENT') {
+			await installSAPUI5()
+		} else {
+			throw readFileError
+		}
+	}
 })()
