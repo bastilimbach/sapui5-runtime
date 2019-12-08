17b955bb046c044b2d8389ca36d85e8786edabd8
Sebastian Limbach <bastilimbach@gmail.com>
2019-12-08 01:02:25 +0100

    Add progress bars

--- src/install.js
+++ src/install.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
+const ProgressBar = require('progress')
 const Downloader = require('./Downloader')
 
 const packageJSONPath = path.resolve(`${__dirname}/../../../package.json`)
@@ -13,7 +14,37 @@ try {
 
 const libDir = path.resolve(`${__dirname}/../lib`)
 const tmpDir = path.resolve(`${__dirname}/../tmp`)
-let selectedSAPUI5Version = config.version;
+let selectedSAPUI5Version = config.version
+
+async function installSAPUI5(downloader) {
+  let downloadProgressBar
+  let extractionProgressBar
+  const zipFile = await downloader.downloadSAPUI5(selectedSAPUI5Version, (total, progress) => {
+    if (downloadProgressBar instanceof ProgressBar) {
+      downloadProgressBar.tick(progress)
+    } else {
+      downloadProgressBar = new ProgressBar('Downloading: [:bar] :rate/bps :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total,
+      })
+    }
+  })
+
+  await downloader.extractSAPUI5(zipFile, (total, progress) => {
+    if (extractionProgressBar instanceof ProgressBar) {
+      extractionProgressBar.tick(progress)
+    } else {
+      extractionProgressBar = new ProgressBar('Extracting: [:bar] :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total,
+      })
+    }
+  })
+}
 
 (async () => {
   const downloader = new Downloader('Runtime', tmpDir, libDir)
@@ -33,13 +64,11 @@ let selectedSAPUI5Version = config.version;
     if (installedSAPUI5Version === selectedSAPUI5Version) {
       console.log(`SAPUI5 version ${installedSAPUI5Version} already installed.`)
     } else {
-      const zipFile = await downloader.downloadSAPUI5(selectedSAPUI5Version)
-      await downloader.extractSAPUI5(zipFile)
+      await installSAPUI5(downloader)
     }
   } catch (readFileError) {
     if (readFileError.code === 'ENOENT') {
-      const zipFile = await downloader.downloadSAPUI5(selectedSAPUI5Version)
-      await downloader.extractSAPUI5(zipFile)
+      await installSAPUI5(downloader)
     } else {
       throw readFileError
     }
